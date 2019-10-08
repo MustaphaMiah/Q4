6cb5d169b1b6b4dc08ed3de74b19f9453f6c19e0
Muhammad Mustapha Miah <muhammad_mustapha_miah@live.co.uk>
2019-10-08 20:56:33 +0100

    fixed JS

--- client.js
+++ client.js
@@ -3,7 +3,12 @@ $(document).ready(function(){
 		template = Handlebars.compile(newsSource);
 
 	$.getJSON('release.json', function(json) {
-		var data = template(json);
+    json.releases = json.releases.sort(function(a,b){
+      // Turn your strings into dates, and then subtract them
+      // to get a value that is either negative, positive, or zero.
+      return new Date(b.date) - new Date(a.date);
+    }).slice(0, 3);
+    var data = template(json);
 		$('.news').html(data);
 	});
 
@@ -13,5 +18,5 @@ $(document).ready(function(){
 
 	Handlebars.registerHelper('truncate', function(text, num) {
 		return text.substr(0,num) + '...';
-	});
+  });
 });
